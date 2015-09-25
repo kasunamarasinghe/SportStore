c97aff29d6c1b25908f2d9075a89f5106c042765
kasunamarasinghe <kasun@davton.com>
2015-09-25 09:56:45 +0530

    Created adding new product info

--- SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -45,5 +45,10 @@ namespace SportsStore.WebUI.Controllers
                 return View(product);
             }
         }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
     }
 }
